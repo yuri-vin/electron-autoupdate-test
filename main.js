16b7a76c83c6b9d862dad2ca96d3325d7527ea7b
yuri-vin <yuri.vinogradoff@gmail.com>
2020-11-11 17:19:43 +0200

    added autoupdater

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, autoUpdater, dialog } = require('electron');
+const isDev = require('electron-is-dev');
+require('update-electron-app')();
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -8,22 +10,37 @@ function createWindow() {
       enableRemoteModule: true,
       nodeIntegration: true
     }
-  })
+  });
 
-  win.loadFile('index.html')
+  win.loadFile('index.html');
   //win.webContents.openDevTools()
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+});
 
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow()
   }
+});
+
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Restart', 'Later'],
+    title: 'Application Update',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+  }
+
+  dialog.showMessageBox(dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0)
+      autoUpdater.quitAndInstall();
+  })
 })
